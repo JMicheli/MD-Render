41faba5dd5df65bbe2565c740ae85d0a7f9e9efb
Joseph Micheli <josephwmicheli@gmail.com>
2022-06-20 21:40:09 -0500

    Fix triangle being upside down like an idiot

--- engine/src/graphics_context/context.rs
+++ engine/src/graphics_context/context.rs
@@ -117,17 +117,17 @@ impl MdrGraphicsContext {
     // TODO remove
     let vertices = [
       Vertex {
-        position: [-0.5, -0.5, 0.0],
+        position: [-0.5, 0.5, 0.0],
         normal: [0.0, 0.0, 0.0],
         color: [0.0, 0.0, 1.0, 1.0],
       },
       Vertex {
-        position: [0.5, -0.5, 0.0],
+        position: [0.5, 0.5, 0.0],
         normal: [0.0, 0.0, 0.0],
         color: [0.0, 1.0, 0.0, 1.0],
       },
       Vertex {
-        position: [0.0, 0.5, 0.0],
+        position: [0.0, -0.5, 0.0],
         normal: [0.0, 0.0, 0.0],
         color: [1.0, 0.0, 0.0, 1.0],
       },
