dd631b7e5a20f96acfbb5c9b4dec2c8dd9430f03
Joseph Micheli <josephwmicheli@gmail.com>
2022-06-25 17:36:10 -0500

    Improved readme, turned off debug by default

--- Readme.md
+++ Readme.md
@@ -1,3 +1,11 @@
 # MD Renderer
 
 A from-scratch engine using the Vulkano crate to safely interact with the Vulkan API.
+
+## Running the example
+
+After cloning the repository, enter the new directory and run the example using:
+
+`cargo run`
+
+Use the left and right keys to rotate the camera.
--- example/src/main.rs
+++ example/src/main.rs
@@ -8,7 +8,7 @@ use mdr_engine::{MdrEngine, MdrEngineOptions, MdrSceneObject};
 fn main() {
   logger::init().expect("Failed to initialize logger");
 
-  let opts = MdrEngineOptions { debug: true };
+  let opts = MdrEngineOptions { debug: false };
   let (mut engine, event_loop) = MdrEngine::new(opts);
 
   // Ground plane
